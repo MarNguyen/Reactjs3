98bde8a61f506cc2c444a5a90832ece2b5314423
Mar Nguyen <minhcacnguyen228@gmail.com>
2022-12-30 23:07:45 +0700

    add code

---
+++ ReactJs3/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+// import Baitap1 from "./baitap1";
+// import Baitap2 from "./baitap2";
+import RenderingElements from "./rendering-elements";
+import HandlingEvents from "./handling-events";
+import State from "./state";
+import RenderColorCar from "./renderColorCar";
+import ListKeys from "./list-keys";
+import Communicate from "./communicate";
+import Shoes from "./shoes";
+import CartDemo from "./CartDemo/CartDemo";
+
+function App() {
+  return (
+    <div>
+      {/* <Baitap1 /> */}
+      {/* <Baitap2 /> */}
+      {/* <RenderingElements />
+      <hr />
+      <HandlingEvents />
+      <hr />
+      <State />
+      <hr />
+      <RenderColorCar />
+      <hr />
+      <ListKeys />
+      <hr />
+      <Communicate />
+      <hr />
+      <Shoes />
+      <br /> 
+      <hr/> */}
+      <CartDemo />
+     
+    </div>
+  );
+}
+
+export default App;
